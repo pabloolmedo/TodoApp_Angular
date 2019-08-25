cfd38b6c2d634037f76789b33e4a4e6e608ebad8
Pablo <pablo.olmedo15@gmail.com>
2019-08-25 20:53:15 -0300

    task components, add button, reactive forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ComponentHomeComponent } from './pages/component-home/component-home.component';
+import { MyTasksComponent } from './pages/my-tasks/my-tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: ComponentHomeComponent
+  },
+  {
+    path: 'mytask',
+    component: MyTasksComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '** ',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedModule } from './shared/shared.module';
+
+import { ComponentHomeComponent } from './pages/component-home/component-home.component';
+import { MyTasksComponent } from './pages/my-tasks/my-tasks.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentHomeComponent,
+    MyTasksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
